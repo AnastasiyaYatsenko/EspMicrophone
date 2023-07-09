a923d8166bd6624703a4ec6b2b91e73adbc92fa5
AnastasiyaYatsenko <62345631+AnastasiyaYatsenko@users.noreply.github.com>
2023-07-10 00:25:33 +0300

    Server
    1. Receives data from sensors and outputs them
    2. Looks at zones of loud sound, groups consecutive

---
+++ EspMicrophone_server/EspMicrophone_server.ino
@@ -0,0 +1,89 @@
+/*
+ * СЕРВЕР
+ * СВЯЗЬ ПО СЕТИ: меш-сеть (painlessMesh)
+ * ФУНКЦИОНАЛ: 1. Принимает данные от датчиков и выводит их
+ *             2. Смотрит зоны громкого звука, группирует подряд идущие
+ */
+
+#include "painlessMesh.h"
+#include <Arduino_JSON.h>
+#include "VolAnalyzer.h"
+
+#define   MESH_PREFIX     "EspMicrophoneMesh"
+#define   MESH_PASSWORD   "somethingSneaky"
+#define   MESH_PORT       5555
+
+const int NODES_COUNT = 4;
+unsigned int net_state[NODES_COUNT][2];
+
+Scheduler     userScheduler;
+painlessMesh  mesh;
+
+void receivedCallback( uint32_t from, String &msg ) {
+  Serial.printf("Received from %u msg=%s\n", from, msg.c_str());
+  JSONVar myObject = JSON.parse(msg.c_str());
+  int node = myObject["node"];
+  double sound = myObject["sound"];
+  double node_time = myObject["time"];
+  Serial.print("Node: ");
+  Serial.print(node);
+  Serial.print("; Sound amplitude: ");
+  Serial.print(sound);
+  Serial.print("; Time: ");
+  Serial.println(node_time);
+
+  net_state[node-1][1] = sound; net_state[node-1][2] = node_time;
+}
+
+void newConnectionCallback(uint32_t nodeId) {
+  Serial.printf("--> startHere: New Connection, nodeId = %u\n", nodeId);
+}
+
+void changedConnectionCallback() {
+  Serial.printf("Changed connections\n");
+}
+
+void nodeTimeAdjustedCallback(int32_t offset) {
+  Serial.printf("Adjusted time %u. Offset = %d\n", mesh.getNodeTime(),offset);
+}
+
+void setup() {
+  Serial.begin(115200);
+
+  mesh.setDebugMsgTypes( ERROR | STARTUP );  // set before init() so that you can see startup messages
+
+  mesh.init( MESH_PREFIX, MESH_PASSWORD, &userScheduler, MESH_PORT );
+  mesh.onReceive(&receivedCallback);
+  mesh.onNewConnection(&newConnectionCallback);
+  mesh.onChangedConnections(&changedConnectionCallback);
+  mesh.onNodeTimeAdjusted(&nodeTimeAdjustedCallback);
+}
+
+void loop() {
+  mesh.update();
+  String msg = "There is a loud noise in areas of sensors: ";
+  boolean is_area = false;
+  for (int i=0; i<NODES_COUNT; i++){
+    uint32_t time_passed = 0;
+    uint32_t my_time = mesh.getNodeTime();
+    //min = 60 000 000 microsec
+    if (my_time<net_state[i][2]){    time_passed = my_time+(71*60000000-net_state[i][2]);    }
+    else {    time_passed = my_time-net_state[i][2];    }
+    if (time_passed>=60000000){
+      net_state[i][1] = 0;
+    }
+    if (net_state[i][1]>0){
+      if (!is_area){
+        msg += "(";
+        is_area = true;
+      }
+      msg += " " + String(i+1);
+    }
+    else {
+      if (is_area){
+        msg += " )";
+        is_area = false;
+      }
+    }
+  }
+}
